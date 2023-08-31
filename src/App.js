195a1eea028d43d317f077cf907fff7a5ad74ac9
habibhardy <habibhardy10@gmail.com>
2023-08-31 23:24:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Tasks from './components/Tasks';
 import TaskForm from './components/TaskForm';
 
 function App() {
+
+
+  const [input, setInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [editTask, seteditTask] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-            THIS IS THE SETTUP APP
-             <TaskForm />
-             <Tasks />
+             <TaskForm input={input} setInput={setInput} tasks={tasks} setTasks={setTasks} editTask={editTask} seteditTask={seteditTask}   />
+             <Tasks  tasks={tasks} setTasks={setTasks}  seteditTask={seteditTask}  />
       </header>
     </div>
   );
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React,{useEffect} from 'react';
 import '../styles/TaskForm.css'
 
-const TaskForm = () => {
+const TaskForm = ({input,setInput,tasks,setTasks,seteditTask,editTask}) => {
+    
+      const onInputChange = (e) =>{
+          setInput(e.target.value);
+      };
+
+
+      useEffect(()=>{
+        if (editTask) {
+            setInput(editTask.title);
+        }else{
+           setInput('');
+        }
+       },[setInput,editTask]);
+
+      const updateTask = (title,id,statu) => {
+               const newTask = tasks.map((task_)=>{
+                   return   task_.id === id ? { title , id , statu } : task_ ;
+               })
+               setTasks(newTask);
+               seteditTask("");
+      }
+
+      const onSubmit = (e) =>{
+        e.preventDefault();
+        if(input === ''){
+            return;
+        }
+        if (!editTask) {
+            setTasks([...tasks,{id:Math.floor(Math.random() * 10000),title:input,statu:false}]);
+            setInput('');
+        }else{
+           updateTask(input,editTask.id,editTask.statu);
+        }
+    };
+
+
+
   return (
-    <div  className='form_task' >
-        TaskForm
-    </div>
+ 
+        <form onSubmit={onSubmit} className='form'>
+
+             <span  className='text' >SAVE TASK</span>
+        
+          <input
+            placeholder='Add a task ...'
+            value={input}
+            onChange={onInputChange}
+            name='text'
+            style={{border:`4px solid ${editTask?`rgb(96, 19, 148)`:``}`}}
+            className='input_'
+          />
+          <button type='submit' className='btn'>
+                  Save
+          </button>
+        
+    </form>
+    
   )
 }
 
